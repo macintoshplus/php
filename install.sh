c4102126ae2f09e08ee0be72b776b853d2f65891
Macintoshplus <814683+macintoshplus@users.noreply.github.com>
2017-10-17 10:51:20 +0200

    ajout de l'extension bc-math

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ apt-key add /root/sury.gpg
 echo "deb https://packages.sury.org/php/ stretch main" > /etc/apt/sources.list.d/sury-php.list
 
 # PHP
-apt-get update && apt-get upgrade -y && apt-get -y install php7.1-dev php7.1-cli php7.1-curl php-pear php7.1-imagick php7.1-gd php7.1-mcrypt php7.1-mbstring php7.1-mysql php7.1-sqlite3 php7.1-xmlrpc php7.1-xsl php7.1-apcu php7.1-ldap php7.1-gmp php7.1-intl php-redis php7.1-zip php7.1-soap php7.1-xml php7.1-common && apt-get autoremove -y && apt-get autoclean && apt-get clean
+apt-get update && apt-get upgrade -y && apt-get -y install php7.1-dev php7.1-cli php7.1-bcmath php7.1-curl php-pear php7.1-imagick php7.1-gd php7.1-mcrypt php7.1-mbstring php7.1-mysql php7.1-sqlite3 php7.1-xmlrpc php7.1-xsl php7.1-apcu php7.1-ldap php7.1-gmp php7.1-intl php-redis php7.1-zip php7.1-soap php7.1-xml php7.1-common && apt-get autoremove -y && apt-get autoclean && apt-get clean
 sed -i 's/\;date\.timezone\ \=/date\.timezone\ \=\ Europe\/Paris/g' /etc/php/7.1/cli/php.ini
 sed -i 's/\memory_limit\ \=\ 128M/memory_limit\ \=\ -1/g' /etc/php/7.1/cli/php.ini
 sed -i 's/\display_errors\ \=\ Off/display_errors\ \=\ On/g' /etc/php/7.1/cli/php.ini
