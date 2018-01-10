cc00a4619add919e15df30161f3b7f5eedc08505
Macintoshplus <814683+macintoshplus@users.noreply.github.com>
2018-01-10 09:13:20 +0100

    add imagick dependency

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ apt-key add /root/sury.gpg
 echo "deb https://packages.sury.org/php/ stretch main" > /etc/apt/sources.list.d/sury-php.list
 
 # PHP
-apt-get update && apt-get upgrade -y && apt-get -y install php7.2-dev php7.2-cli php7.2-bcmath php7.2-curl php-pear php7.2-gd php7.2-mbstring php7.2-mysql php7.2-sqlite3 php7.2-xmlrpc php7.2-xsl php7.2-ldap php7.2-gmp php7.2-intl php7.2-zip php7.2-soap php7.2-xml php7.2-common php7.2-json php7.2-opcache php7.2-readline
+apt-get update && apt-get upgrade -y && apt-get -y install php7.2-dev php7.2-cli php7.2-bcmath php7.2-curl php-pear php7.2-gd php7.2-mbstring php7.2-mysql php7.2-sqlite3 php7.2-xmlrpc php7.2-xsl php7.2-ldap php7.2-gmp php7.2-intl php7.2-zip php7.2-soap php7.2-xml php7.2-common php7.2-json php7.2-opcache php7.2-readline libgraphicsmagick1-dev
 # Disabled ext from repos :  php7.2-imagick php7.2-xdebug php7.2-apcu
 # Disabled php-redis
 
@@ -23,7 +23,7 @@ wget https://github.com/pear/Archive_Tar/releases/download/1.4.3/Archive_Tar-1.4
 tar -xvf Archive_Tar-1.4.3.tgz
 cp Archive_Tar-1.4.3/Archive/Tar.php /usr/share/php/Archive/Tar.php
 
-pecl install sqlsrv-5.1.1preview && pecl install pdo_sqlsrv-5.1.1preview
+pecl install sqlsrv-5.2.0RC1 && pecl install pdo_sqlsrv-5.2.0RC1
 echo "extension=sqlsrv" > /etc/php/7.2/mods-available/sqlsrv.ini
 echo "extension=pdo_sqlsrv" > /etc/php/7.2/mods-available/pdo_sqlsrv.ini
 
