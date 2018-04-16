b2f4666c274df024938f92481c90fbaf1aa5e988
Macintoshplus <814683+macintoshplus@users.noreply.github.com>
2018-04-16 09:42:59 +0200

    Fix error SSL 1.0 install

--- install.sh
+++ install.sh
@@ -17,8 +17,8 @@ apt-get update && apt-get -y upgrade && apt-get install -y mysql-client msodbcsq
 
 # Fix SQLSTATE[01000]: [unixODBC][Driver Manager]Can't open lib '/opt/microsoft/msodbcsql/lib64/libmsodbcsql-13.1.so.9.1' : file not found
 wget http://security.debian.org/debian-security/pool/updates/main/o/openssl/libssl1.0.0_1.0.1t-1+deb7u4_amd64.deb
-dpkg -i libssl1.0.0_1.0.1t-1+deb7u3_amd64.deb
-rm libssl1.0.0_1.0.1t-1+deb7u3_amd64.deb
+dpkg -i libssl1.0.0_1.0.1t-1+deb7u4_amd64.deb
+rm libssl1.0.0_1.0.1t-1+deb7u4_amd64.deb
 
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> /root/.bash_profile && echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> /root/.bashrc && chmod +x /root/.bashrc
 /root/.bashrc
