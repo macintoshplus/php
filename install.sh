3f58759f92b86be19aa9f1fe3429141cc01fdc9e
Macintoshplus <814683+macintoshplus@users.noreply.github.com>
2018-06-01 10:20:59 +0200

    fix build

--- install.sh
+++ install.sh
@@ -52,4 +52,4 @@ phpenmod -v 7.2 -s cli amqp sqlsrv pdo_sqlsrv redis xdebug imagick
 
 useradd -s /bin/bash --home /sources --no-create-home phpuser
 
-apt-get remove -y libgcc-6-dev libgcc-7-dev libmagickwand-dev libmagickcore-dev && apt autoremove -y
+apt-get remove -y libgcc-6-dev libmagickwand-dev libmagickcore-dev && apt autoremove -y
