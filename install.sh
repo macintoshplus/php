112ba1278ea7b552514dcdf5f3bbf288d9587220
Macintoshplus <814683+macintoshplus@users.noreply.github.com>
2018-09-04 09:13:47 +0200

    Add Zip

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Jb Nahan Base container
 ##
 
-apt-get update && apt-get -y upgrade && apt-get -y install curl wget locales nano git subversion sudo librabbitmq-dev xfonts-75dpi libfontconfig1 libjpeg62-turbo libxrender1 xfonts-base fontconfig unixodbc-dev apt-transport-https gnupg locales-all libssl1.0.2 pkg-config libmagickwand-dev
+apt-get update && apt-get -y upgrade && apt-get -y install curl wget locales nano git subversion sudo librabbitmq-dev xfonts-75dpi libfontconfig1 libjpeg62-turbo libxrender1 xfonts-base fontconfig unixodbc-dev apt-transport-https gnupg locales-all libssl1.0.2 pkg-config libmagickwand-dev zip
 apt-get -y install pdftk 
 # Add Source List
 
