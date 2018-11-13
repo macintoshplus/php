bd3b3f010682f5add5172052765dfb0d519986eb
Macintoshplus <814683+macintoshplus@users.noreply.github.com>
2018-11-13 11:54:42 +0100

    update for PHP 7.3

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ##
-# Jb Nahan PHP 7.2 container
+# Jb Nahan PHP 7.3 container
 ##
 
 export DEBIAN_FRONTEND=noninteractive
@@ -13,15 +13,15 @@ wget -q -O - https://packagecloud.io/gpg.key | sudo apt-key add -
 echo "deb http://packages.blackfire.io/debian any main" >/etc/apt/sources.list.d/blackfire.list
 
 # PHP
-apt-get update && apt-get upgrade -y && apt-get -y install php7.2-dev php7.2-cli php7.2-bcmath php7.2-curl php-pear php7.2-gd php7.2-mbstring php7.2-mysql php7.2-sqlite3 php7.2-xmlrpc php7.2-xsl php7.2-ldap php7.2-gmp php7.2-intl php7.2-zip php7.2-soap php7.2-xml php7.2-common php7.2-json php7.2-opcache php7.2-readline blackfire-agent blackfire-php
-# Disabled ext from repos :  php7.2-imagick php7.2-xdebug php7.2-apcu
+apt-get update && apt-get upgrade -y && apt-get -y install php7.3-dev php7.3-cli php7.3-bcmath php7.3-curl php-pear php7.3-gd php7.3-mbstring php7.3-mysql php7.3-sqlite3 php7.3-xmlrpc php7.3-xsl php7.3-ldap php7.3-gmp php7.3-intl php7.3-zip php7.3-soap php7.3-xml php7.3-common php7.3-json php7.3-opcache php7.3-readline blackfire-agent blackfire-php
+# Disabled ext from repos :  php7.3-imagick php7.3-xdebug php7.3-apcu
 # Disable dependencies : libmagickwand-6.q16-dev
 # Disabled php-redis
 
-sed -i 's/\;date\.timezone\ \=/date\.timezone\ \=\ Europe\/Paris/g' /etc/php/7.2/cli/php.ini
-sed -i 's/\memory_limit\ \=\ 128M/memory_limit\ \=\ -1/g' /etc/php/7.2/cli/php.ini
-sed -i 's/\display_errors\ \=\ Off/display_errors\ \=\ On/g' /etc/php/7.2/cli/php.ini
-sed -i 's/disable_functions\ \=\ pcntl_alarm,pcntl_fork,pcntl_waitpid,pcntl_wait,pcntl_wifexited,pcntl_wifstopped,pcntl_wifsignaled,pcntl_wexitstatus,pcntl_wtermsig,pcntl_wstopsig,pcntl_signal,pcntl_signal_dispatch,pcntl_get_last_error,pcntl_strerror,pcntl_sigprocmask,pcntl_sigwaitinfo,pcntl_sigtimedwait,pcntl_exec,pcntl_getpriority,pcntl_setpriority,/\;disable_functions\ \=\ pcntl_alarm,pcntl_fork,pcntl_waitpid,pcntl_wait,pcntl_wifexited,pcntl_wifstopped,pcntl_wifsignaled,pcntl_wexitstatus,pcntl_wtermsig,pcntl_wstopsig,pcntl_signal,pcntl_signal_dispatch,pcntl_get_last_error,pcntl_strerror,pcntl_sigprocmask,pcntl_sigwaitinfo,pcntl_sigtimedwait,pcntl_exec,pcntl_getpriority,pcntl_setpriority,/g' /etc/php/7.2/cli/php.ini
+sed -i 's/\;date\.timezone\ \=/date\.timezone\ \=\ Europe\/Paris/g' /etc/php/7.3/cli/php.ini
+sed -i 's/\memory_limit\ \=\ 128M/memory_limit\ \=\ -1/g' /etc/php/7.3/cli/php.ini
+sed -i 's/\display_errors\ \=\ Off/display_errors\ \=\ On/g' /etc/php/7.3/cli/php.ini
+sed -i 's/disable_functions\ \=\ pcntl_alarm,pcntl_fork,pcntl_waitpid,pcntl_wait,pcntl_wifexited,pcntl_wifstopped,pcntl_wifsignaled,pcntl_wexitstatus,pcntl_wtermsig,pcntl_wstopsig,pcntl_signal,pcntl_signal_dispatch,pcntl_get_last_error,pcntl_strerror,pcntl_sigprocmask,pcntl_sigwaitinfo,pcntl_sigtimedwait,pcntl_exec,pcntl_getpriority,pcntl_setpriority,/\;disable_functions\ \=\ pcntl_alarm,pcntl_fork,pcntl_waitpid,pcntl_wait,pcntl_wifexited,pcntl_wifstopped,pcntl_wifsignaled,pcntl_wexitstatus,pcntl_wtermsig,pcntl_wstopsig,pcntl_signal,pcntl_signal_dispatch,pcntl_get_last_error,pcntl_strerror,pcntl_sigprocmask,pcntl_sigwaitinfo,pcntl_sigtimedwait,pcntl_exec,pcntl_getpriority,pcntl_setpriority,/g' /etc/php/7.3/cli/php.ini
 
 pecl channel-update pecl.php.net
 ## Fix Tar Error
@@ -30,29 +30,29 @@ tar -xvf Archive_Tar-1.4.3.tgz
 cp Archive_Tar-1.4.3/Archive/Tar.php /usr/share/php/Archive/Tar.php
 
 pecl install sqlsrv-5.2.0 && pecl install pdo_sqlsrv-5.2.0
-echo "extension=sqlsrv" > /etc/php/7.2/mods-available/sqlsrv.ini
-echo "extension=pdo_sqlsrv" > /etc/php/7.2/mods-available/pdo_sqlsrv.ini
+echo "extension=sqlsrv" > /etc/php/7.3/mods-available/sqlsrv.ini
+echo "extension=pdo_sqlsrv" > /etc/php/7.3/mods-available/pdo_sqlsrv.ini
 
 #PEAR
 pear upgrade && pear install pecl/amqp-1.9.3
-echo "extension=amqp" > /etc/php/7.2/mods-available/amqp.ini
+echo "extension=amqp" > /etc/php/7.3/mods-available/amqp.ini
 
 
 pear install pecl/redis
-echo "extension=redis" > /etc/php/7.2/mods-available/redis.ini
+echo "extension=redis" > /etc/php/7.3/mods-available/redis.ini
 
 apt install -y libmagickwand-dev libmagickcore-dev libmagickwand-6.q16-3 libmagickcore-6.q16-3
 pear install pecl/imagick
-echo "extension=imagick" > /etc/php/7.2/mods-available/imagick.ini
+echo "extension=imagick" > /etc/php/7.3/mods-available/imagick.ini
 
 #git clone git://github.com/xdebug/xdebug.git
 #cd xdebug
 #git co 2.6.0beta1
-#/usr/bin/phpize7.2 && ./configure --enable-xdebug && make && make install
+#/usr/bin/phpize7.3 && ./configure --enable-xdebug && make && make install
 pear install pecl/xdebug
-echo "zend_extension=xdebug" > /etc/php/7.2/mods-available/xdebug.ini
+echo "zend_extension=xdebug" > /etc/php/7.3/mods-available/xdebug.ini
 
-phpenmod -v 7.2 -s cli amqp sqlsrv pdo_sqlsrv redis xdebug imagick
+phpenmod -v 7.3 -s cli amqp sqlsrv pdo_sqlsrv redis xdebug imagick
 # Disable : imagick
 
 useradd -s /bin/bash --home /sources --no-create-home phpuser
