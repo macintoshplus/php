aaecd335f3dd6ce54036b1956b36f2f5dcb8d490
Jean-Baptiste Nahan <814683+macintoshplus@users.noreply.github.com>
2019-03-14 14:38:43 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ apt-key add /root/sury.gpg
 echo "deb https://packages.sury.org/php/ stretch main" > /etc/apt/sources.list.d/sury-php.list
 
 # Blackfire
-wget -q -O - https://packagecloud.io/gpg.key | sudo apt-key add -
-echo "deb http://packages.blackfire.io/debian any main" >/etc/apt/sources.list.d/blackfire.list
+wget -q -O - https://packages.blackfire.io/gpg.key | sudo apt-key add -
+echo "deb http://packages.blackfire.io/debian any main" | sudo tee /etc/apt/sources.list.d/blackfire.list
 
 # PHP
 apt-get update && apt-get upgrade -y && apt-get -y install php7.2-dev php7.2-cli php7.2-bcmath php7.2-curl php-pear php7.2-gd php7.2-mbstring php7.2-mysql php7.2-sqlite3 php7.2-xmlrpc php7.2-xsl php7.2-ldap php7.2-gmp php7.2-intl php7.2-zip php7.2-soap php7.2-xml php7.2-common php7.2-json php7.2-opcache php7.2-readline blackfire-agent blackfire-php
